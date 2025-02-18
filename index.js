268fa8d9302348617dfdd87750b9b8056e617835
tan <vct3012.git@gmail.com>
2025-02-18 13:36:35 +0700

    Testing

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = "./data.json";
 
 const isValidDate = (date) => {
   const startDate = moment("2022-01-01");
-  const endDate = moment("2025-01-07"); // Cập nhật ngày kết thúc
+  const endDate = moment("2025-02-18"); // Cập nhật ngày kết thúc
   return date.isBetween(startDate, endDate, null, "[]");
 };
 
